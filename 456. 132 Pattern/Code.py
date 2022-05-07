fdb6f49b0dcfcbc76aad8e9e8c7c0be92586d966
Krishna Raj <75441878+iam-krj@users.noreply.github.com>
2022-05-07 10:58:28 -0700

    7th May

---
+++ 456. 132 Pattern/Code.py
@@ -0,0 +1,17 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        second_max = -math.inf
+        
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i] < second_max:
+                return True
+            
+            while stack and nums[i] > stack[-1]:
+                ele = stack.pop()
+                second_max = max(second_max,ele)
+            
+            #All peak values are inserted into the stack
+            stack.append(nums[i])
+            
+        return False
